386f8235d5bd0f12cf8e74dd11f37f130f90604e
Tomas Perez <tomasitopg6@gmail.com>
2020-07-03 08:28:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,9 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+      # more code coming soon!
+  end
   def print_courses
     self.make_courses
     Course.all.each do |course|
